a00993d4fecc4c800865331a55521d0afe6df08c
TinTinWinata <tintin6892@gmail.com>
2022-07-31 08:50:10 +0700

    Added GQLGen And Custom Model

--- backend/go.mod
+++ backend/go.mod
@@ -3,28 +3,31 @@ module go/main
 go 1.18
 
 require (
+	github.com/99designs/gqlgen v0.17.13
 	github.com/gin-gonic/gin v1.8.1
-	github.com/go-pg/pg v8.0.7+incompatible
 	github.com/uptrace/bun v1.1.7
 	github.com/uptrace/bun/dialect/pgdialect v1.1.7
 	github.com/uptrace/bun/driver/pgdriver v1.1.7
+	github.com/vektah/gqlparser/v2 v2.4.6
 )
 
 require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.3.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.20.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
@@ -36,5 +39,6 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	mellium.im/sasl v0.2.1 // indirect
 )
--- backend/server.go
+++ backend/server.go
@@ -1,122 +1,29 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
+	"go/main/graph"
+	"go/main/graph/generated"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-// PG WITH BUN
-
-// Cara HE
-// func handleRoute(w http.ResponseWriter, r *http.Request){
-//  fmt.Fprintln(w, "<h1>Ini halo dari go</h1>")
-// }
-
-// func main() {
-//  http.HandleFunc("/", handleRoute)
-//  err := http.ListenAndServe(":4321", nil)
-//  if err != nil{
-//   fmt.Println(err)
-//  }
-// }
-
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-type User struct {
-	bun.BaseModel `bun:"table:users"`
-	Id            uint   `bun:"user_id,pk,autoincrement" `
-	Email         string `bun:"email,notnull"`
-	Name          string `bun:"name,notnull"`
-	Password      string `bun:"password,notnull"`
-}
+const defaultPort = "8080"
 
 func main() {
-	dsn := "postgres://postgres:asd@localHost:5432/postgres?sslmode=disable"
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-	db := bun.NewDB(sqldb, pgdialect.New())
-
-	ctx := context.Background()
-
-	// GET
-
-	// Ngambil 1 doang
-	// user := new(User)
-	// err := db.NewSelect().Model(user).Scan(ctx)
-
-	// Ngambil lebih dari 1
-	users := []User{}
-	err := db.NewSelect().Model(&users).Scan(ctx)
-
-	if err != nil {
-		panic(err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
 
-	fmt.Println(users)
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	// POST
-	// newuser := &User{
-	// 	Name:     "Vito Gustianto",
-	// 	Email:    "vito@gmail.com",
-	// 	Password: "inipassword",
-	// }
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
-	// result, err := db.NewInsert().Model(newuser).Exec(ctx)
-
-	// if err != nil {
-	// panic(err)
-	// }
-
-	// fmt.Println(result)
-
-	// PUT
-	// changedUser := &User{
-	// 	Id:       2,
-	// 	Name:     "Justine Winata",
-	// 	Email:    "justine@gmail.com",
-	// 	Password: "justinewinata",
-	// }
-
-	// res, err := db.NewUpdate().Model(changedUser).WherePK().Exec(ctx)
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(res)
-
-	// DELETE
-
-	// res, err := db.NewDelete().Model(&User{}).Where("user_id = ?", 3).Exec(ctx)
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(res)
-
-	router := gin.Default()
-	router.GET("/album", getAlbums)
-	router.Run("localhost:8080")
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
